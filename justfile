21fc34ba8c4032cefd6af5179d35f4ed630041d7
Sinclair Target <sinclairtarget@gmail.com>
2025-11-29 17:15:35 -0500

    Add version scaffolding.

--- justfile
+++ justfile
@@ -1,7 +1,9 @@
 executable_name := "michel"
+version := `git describe --tags --always --dirty`
 
 build:
-    go build -o {{executable_name}}
+    go build -o {{executable_name}} \
+        -ldflags '-X main.Version={{version}} -X main.BuildTag=dev'
 
 clobber:
     rm -f {{executable_name}}
--- main.go
+++ main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
+var Version string  // Semantic version
+var BuildTag string // Optional tag
+
 func main() {
-	fmt.Println("Hello, world!");
+	mainFlagSet := flag.NewFlagSet("michel", flag.ExitOnError)
+
+	versionFlag := mainFlagSet.Bool("version", false, "Print version and exit")
+
+	mainFlagSet.Usage = func() {
+		fmt.Println("Usage: michel [subcommand] [subcommand options...]")
+		fmt.Println("       michel --version")
+		fmt.Println("michel builds websites from MyST markdown")
+
+		fmt.Println()
+		fmt.Println("Top-level options:")
+		mainFlagSet.PrintDefaults()
+	}
+
+	mainFlagSet.Parse(os.Args[1:])
+
+	if *versionFlag {
+		fmt.Println(getVersionString())
+		return
+	}
+}
+
+func getVersionString() string {
+	if (Version == "") {
+		return "unknown"
+	}
+
+	if (BuildTag != "") {
+		return fmt.Sprintf("%s-%s", Version, BuildTag)
+	}
+
+	return Version;
 }
