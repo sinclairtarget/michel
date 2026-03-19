3fd011b13934971724160c699df96d52fdecfa18
Sinclair Target <sinclairtarget@gmail.com>
2026-03-19 08:48:45 -0400

    Content frontmatter date parsing.

--- internal/content/content.go
+++ internal/content/content.go
@@ -4,22 +4,50 @@ package content
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/sinclairtarget/michel/internal/content/myst"
 	"github.com/sinclairtarget/michel/internal/frontmatter"
 	"github.com/sinclairtarget/michel/internal/util"
 )
 
-type ContentFrontmatter struct {
-	Title string
+type contentFrontmatter struct {
+	Title       string
+	Description string
+	Date        string
+}
+
+// If Date is missing, fallback to Jan 1, year 1.
+//
+// Hugo does this and it seems like a reasonable choice.
+var fallbackDate time.Time = time.Date(1, 1, 1, 0, 0, 0, 0, time.Local)
+
+// Parses the Date field into a time.Time.
+//
+// We use this to represent the calendar date associated with the content. But
+// this is just a calendar date and not an instant in time.
+func (c contentFrontmatter) ParsedDate() (time.Time, error) {
+	if c.Date == "" {
+		return fallbackDate, nil
+	}
+
+	t, err := time.Parse("2006-01-02", c.Date)
+	if err != nil {
+		return fallbackDate, err
+	}
+
+	return t, nil
 }
 
 // A file with content for the site.
 type Content struct {
-	Key         string // unique id for the content
-	Path        string // path content was loaded from
-	Frontmatter ContentFrontmatter
-	Root        *myst.Node
+	Key  string // unique id for the content
+	Path string // path content was loaded from
+	Root *myst.Node
+	// From frontmatter
+	Title       string
+	Description string
+	Date        time.Time
 }
 
 // Loads content file into memory, parsing the markdown.
@@ -38,12 +66,24 @@ func LoadFromMarkdown(contentDir string, path string) (Content, error) {
 	}
 	defer f.Close()
 
-	result, err := frontmatter.ReadFile[ContentFrontmatter](f)
+	result, err := frontmatter.ReadFile[contentFrontmatter](f)
 	if err != nil {
 		return content, err
 	}
 
-	content.Frontmatter = result.Frontmatter
+	// Load frontmatter fields
+	content.Title = result.Frontmatter.Title
+	content.Description = result.Frontmatter.Description
+
+	content.Date, err = result.Frontmatter.ParsedDate()
+	if err != nil {
+		return content, fmt.Errorf(
+			"failed to parse frontmatter date in content file \"%s\": %w",
+			err,
+		)
+	}
+
+	// Parse MyST
 	content.Root, err = myst.Parse(result.Text)
 	if err != nil {
 		return content, fmt.Errorf(
